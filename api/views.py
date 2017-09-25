21d3598c3fd91d346c832b6d2a48bea0f5219474
Diego Lopes <dihgg@dihgg.com>
2017-09-25 20:38:37 -0300

    Primeira chamada na api do metro

--- api/views.py
+++ api/views.py
@@ -1,7 +1,13 @@
+import requests
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import JsonResponse
 
 
 def index(request):
-    return JsonResponse({'foo': "bar"})
+    return JsonResponse({"foo": "bar"})
+
+
+def getstatus(request):
+    response = requests.get("http://www.viaquatro.com.br/generic/Main/LineStatus")
+    return JsonResponse(response.json())
