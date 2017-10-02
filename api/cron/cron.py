025398b595704c24ce408acb14a9894429abbbe0
Diego Lopes <dihgg@dihgg.com>
2017-10-02 09:46:16 -0300

    Cron error

--- api/cron/cron.py
+++ api/cron/cron.py
@@ -1,14 +1,18 @@
+import logging
+
 from apscheduler.schedulers.blocking import BlockingScheduler
 import requests
 
+logger = logging.getLogger(__name__)
 
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job('interval', minutes=60)
+@sched.scheduled_job('interval', minutes=30)
 def timed_job():
     requests.get("https://aps8train.herokuapp.com/api/update-lines")
     print('CRON - Updated LOG')
+    logger.error("Running CRON")
 
 
 sched.start()
